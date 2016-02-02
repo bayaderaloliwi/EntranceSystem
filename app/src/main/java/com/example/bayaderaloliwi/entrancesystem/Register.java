8d5adf8e224e1d2f77734a07eb2197fca9bd0fdc
nkalsabhan <nks.sabhan@yahoo.com>
2016-02-02 15:19:56 +0300

    edited register

--- app/src/main/java/com/example/bayaderaloliwi/entrancesystem/Register.java
+++ app/src/main/java/com/example/bayaderaloliwi/entrancesystem/Register.java
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Register extends AppCompatActivity {
 
@@ -16,14 +18,11 @@ public class Register extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        Spinner dropdown = (Spinner)findViewById(R.id.spinner);
+        String[] items = new String[]{"1", "2", "three"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
+
     }
 
 }
